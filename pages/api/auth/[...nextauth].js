fd32f902a65ee10da753f72291dc7bfea491dfe3
oheneadj <oheneadjei.dev@gmail.com>
2022-05-19 16:45:34 +0000

    fixed redirect hang

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -45,7 +45,6 @@ export default NextAuth({
 
   callbacks: {
     jwt: {
-      // The maximum age of the NextAuth.js issued JWT in seconds.
       // Defaults to `session.maxAge`.
       maxAge: 60 * 60 * 24 * 30,
       // You can define your own encode/decode functions for signing and encryption
