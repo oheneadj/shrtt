4d1ba9796806ac7dc579562338a0bb6c41fb8dbe
Ohene Adjei <oheneadjei.dev@gmail.com>
2022-05-27 07:53:08 +0000

    Update shortener.js

--- pages/api/url/shortener.js
+++ pages/api/url/shortener.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   //Create a new ShortURL
   if (req.method === "POST") {
     const { longUrl } = req.body;
-    const baseUrl = "https://shrtt.in";
+    const baseUrl = "shrtt.in";
 
     //Check to see if baseURL is a valid URL
     if (!validUrl.isUri(baseUrl)) {
